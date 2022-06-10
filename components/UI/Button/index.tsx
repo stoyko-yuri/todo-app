8b2128de769e0249211ddcead485365a6c4382bd
Yura <45919541+stoyko-yuri@users.noreply.github.com>
2022-06-11 01:13:13 +0300

    Fix styles

--- components/UI/Button/index.tsx
+++ components/UI/Button/index.tsx
@@ -10,7 +10,7 @@ const Button = ({ styles, icon, onClick }: PropsTypes) => {
   return (
     <div
       className={
-        "p-px text-xl grid items-center jusitfy-center rounded-full bg-white cursor-pointer opacity-80 hover:opacity-100 " +
+        "p-px text-xl grid items-center jusitfy-center rounded-full cursor-pointer opacity-80 hover:opacity-100 " +
         styles
       }
       onClick={onClick}
